f2c6ac0899fa29a0cf0dc73adadc94ef10e0e34b
Jaden Accord <jbr.accord@gmail.com>
2023-08-07 19:38:56 +0200

    1480 submission

--- Leetcode/1480-running_sum/main.rs
+++ Leetcode/1480-running_sum/main.rs
@@ -1,16 +1,21 @@
-impl Solution {
-    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
-        let mut sum: [i32, nums.len()];
-        sum[0] = nums[0];
-        for i in 1..(nums.len()) {
-            sum[i] = sum[i - 1] + nums[i];
-        }
-        return sum;
-    }
-}
+// Test case
 
 fn main() {
-    let nums = [1,2,3,4];
-    let sum = running_sum(nums);
-    print(sum);
+    let nums1: Vec<i32> = vec![1, 2, 3, 4];
+    let nums2: Vec<i32> = vec![1, 1, 1, 1, 1];
+    let nums3: Vec<i32> = vec![3, 1, 2, 10, 1];
+    println!("Example 1: {:?}", running_sum(&nums1));
+    println!("Example 2: {:?}", running_sum(&nums2));
+    println!("Example 3: {:?}", running_sum(&nums3));
+}
+
+// Submission 1 (0 ms beats 100%, 2.3 MB beats 13.2%)
+
+fn running_sum(nums: &Vec<i32>) -> Vec<i32> {
+    let mut sum: Vec<i32> = Vec::new();
+    sum.push(nums[0]);
+    for i in 1..(nums.len()) {
+        sum.push(sum[i - 1] + nums[i]);
+    }
+    return sum;
 }
