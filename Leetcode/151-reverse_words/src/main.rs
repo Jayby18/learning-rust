a9668222a735edbe82b782b6875da19597b4f4fe
Jaden Accord <jbr.accord@gmail.com>
2023-08-08 09:23:29 +0200

    151

--- Leetcode/151-reverse_words/src/main.rs
+++ Leetcode/151-reverse_words/src/main.rs
@@ -8,15 +8,15 @@ fn main() {
     println!("Example 3: '{}'", reverse_words("a good   example".to_string()));
 }
 
-// Submission
+// Submission (1 ms beats 84.56%, 2.14 MB beats 55.03%)
+
 fn reverse_words(s: String) -> String {
     let mut new_string: String = String::new();
     let words: Vec<&str> = s.split_whitespace().collect();
-    for word in words.iter().rev() [
-        println!("Word: {}", word.to_string())
+    for word in words.iter().rev() {
         new_string.push_str(word);
         new_string.push(' ');
-    ]
+    }
     new_string.pop();
     return new_string;
 }
