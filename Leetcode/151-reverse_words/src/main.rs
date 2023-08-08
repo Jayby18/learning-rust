c29ab126695ea18457aca4252a4d27c5ef2c2f59
Jaden Accord <jbr.accord@gmail.com>
2023-08-08 03:13:20 +0200

    Init 151

---
+++ Leetcode/151-reverse_words/src/main.rs
@@ -0,0 +1,22 @@
+// Given an input string s, reverse the order of words. Exclude extra whitespaces.
+
+// Test case
+
+fn main() {
+    println!("Example 1: '{}'", reverse_words("the sky is blue".to_string()));
+    println!("Example 2: '{}'", reverse_words("  hello world  ".to_string()));
+    println!("Example 3: '{}'", reverse_words("a good   example".to_string()));
+}
+
+// Submission
+fn reverse_words(s: String) -> String {
+    let mut new_string: String = String::new();
+    let words: Vec<&str> = s.split_whitespace().collect();
+    for word in words.iter().rev() [
+        println!("Word: {}", word.to_string())
+        new_string.push_str(word);
+        new_string.push(' ');
+    ]
+    new_string.pop();
+    return new_string;
+}
